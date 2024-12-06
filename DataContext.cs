50e00812be82a418c95027031b38539268b1a916
Wes <161781912+weslainesantana@users.noreply.github.com>
2024-12-05 23:24:22 -0300

    Add files via upload
    "Feat: atualização"

--- DataContext.cs
+++ DataContext.cs
@@ -10,5 +10,19 @@ namespace CursoOnlineAPI
         public DbSet<Cursos> Cursos { get; set; }
         public DbSet<Alunos> Alunos { get; set; }
         public DbSet<Matrículas> Matrículas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Matrículas>()
+                .HasOne(m => m.Alunos)
+                .WithMany()
+                .HasForeignKey(m => m.AlunoId);
+
+            modelBuilder.Entity<Matrículas>()
+                .HasOne(m => m.Cursos)
+                .WithMany()
+                .HasForeignKey(m => m.CursoId);
+        }
+
     }
 }
